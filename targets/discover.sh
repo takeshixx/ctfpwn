1982c58f9f9ace5fa9223433966dfe9ec442e4fd
takeshix <takeshix@adversec.com>
2015-11-19 00:25:33 +0100

    Update discover.sh

--- targets/discover.sh
+++ targets/discover.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+# This scripts checks which hosts in the CTF range are up.
+# It writes all alive IPs into /srv/targets/alive and calls
+# /srv/ctf-pwn/targets/update.py which updates the status
+# of the alive hosts in the database.
 
 targets="10.60.1-41.2 10.60.43-254.2 10.61.1-234.2"
 
 while :;do
     nmap -sP -PS22 -oG - "${targets}" | grep "Status: Up" | awk '{print $2}' > /srv/targets/alive
-    /srv/targets/update.py
+    /srv/ctf-pwn/targets/update.py
     sleep 10;
 done
