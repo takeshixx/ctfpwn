78ac4bb46f38d8e0df39fe33c94edd90cd60bad4
takeshix <takeshix@adversec.com>
2015-11-19 23:18:20 +0100

    pushing local changes

--- targets/discover.sh
+++ targets/discover.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
-# This scripts checks which hosts in the CTF range are up.
+# This script checks which hosts in the CTF range are up.
 # It writes all alive IPs into /srv/targets/alive and calls
 # /srv/ctf-pwn/targets/update.py which updates the status
 # of the alive hosts in the database.
+#
+# This script will be started manually via Supervisor.
+
+# TODO: Add the ports of all services to the Nmap port list
+# in order to make more reliable alive checks.
 
 targets="10.60.1-41.2 10.60.43-254.2 10.61.1-234.2"
 
 while :;do
-    nmap -sP -PS22 -oG - "${targets}" | grep "Status: Up" | awk '{print $2}' > /srv/targets/alive
+    nmap -sP -PS22,80 -oG - "${targets}" | grep "Status: Up" | awk '{print $2}' > /srv/targets/alive
     /srv/ctf-pwn/targets/update.py
     sleep 10;
 done
