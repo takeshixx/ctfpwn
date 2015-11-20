acfa13e220801055e559aeec550941dbcec505b5
takeshix <takeshix@adversec.com>
2015-11-20 09:37:36 +0100

    fixed

--- targets/discover.sh
+++ targets/discover.sh
@@ -12,7 +12,8 @@
 targets="10.60.1-41.2 10.60.43-254.2 10.61.1-234.2"
 
 while :;do
-    nmap -sP -PS22,80 -oG - "${targets}" | grep "Status: Up" | awk '{print $2}' > /srv/targets/alive
+    eval "nmap -sP -PS22,80 -oG /srv/targets/nmap.out ${targets}"
+    cat /srv/targets/nmap.out | grep "Status: Up" | awk '{print $2}' > /srv/targets/alive
     /srv/ctf-pwn/targets/update.py
     sleep 10;
 done
