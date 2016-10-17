3400b03bed52971cbbab0f05238eb192865ee055
Benjamin <bschwendemann@ernw.de>
2016-10-17 22:54:04 +0200

    Merge class Flag from exploitservice/flag.py into flagservice/flag.py

--- flagservice/flag.py
+++ flagservice/flag.py
@@ -9,3 +9,15 @@ class Flag():
         self.target = incoming[1]
         self.flag = incoming[2]
         self.timestamp = int(time.time())
+
+
+    def __init__(self, service, target, flag):
+        """
+        :type service: str
+        :type target: str
+        :type flag: str
+        """
+        self.service = service
+        self.target = target
+        self.flag = flag
+        self.timestamp = int(time.time())
