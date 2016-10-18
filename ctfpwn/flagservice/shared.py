610d459dd35cdc5580d5f74c1e6d3bb9b6bb018f
takeshix <takeshix@adversec.com>
2016-10-19 00:46:39 +0200

    Only try where it's necessary

--- ctfpwn/flagservice/shared.py
+++
@@ -1,8 +0,0 @@
-"""A module for shared instances."""
-import logging
-from .flagdb import FlagDB
-
-LOG_LEVEL_STAT = logging.INFO + 1
-logging.addLevelName(LOG_LEVEL_STAT, 'STATISTIC')
-
-flag_db = FlagDB()
