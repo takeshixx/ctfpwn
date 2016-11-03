28d05d08990a87757b4234855c9c229666269f0f
takeshix <takeshix@adversec.com>
2016-11-03 10:23:15 +0100

    Make run_api wrapper accessible in ctfpwn

--- ctfpwn/__init__.py
+++ ctfpwn/__init__.py
@@ -1,3 +1,4 @@
 from .exploitservice import run_exploitservice
 from .flagservice import run_flagservice
 from .targets import run_targetservice
+from .api import run_api
--- run-api.py
+++ run-api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from ctfpwn.api import run_api
+from ctfpwn import run_api
 
 if __name__ == '__main__':
     run_api()
