4e9a181f5edbf3018bfcb1906eae34052b533a64
BlueC0re <coding@bluec0re.eu>
2016-11-12 23:39:26 +0100

    implemented lazy imports (works 100% transparent)
    see #22

--- ctfpwn/__init__.py
+++ ctfpwn/__init__.py
@@ -1,4 +1,41 @@
-from .exploitservice import run_exploitservice
-from .flagservice import run_flagservice
-from .targets import run_targetservice
-from .api import run_api
+import sys
+
+
+__all__ = []
+
+
+class LazyImporter:
+    def __init__(self, m):
+        self.__attribs = {}
+        self.__wrapped_module = m
+
+    def add(self, submodule, name):
+        self.__attribs[name] = submodule
+        getattr(self, '__all__').append(name)
+
+    def __getattr__(self, name):
+        try:
+            return getattr(self.__wrapped_module, name)
+        except AttributeError:
+            pass
+
+        import importlib
+
+        try:
+            module = self.__attribs[name]
+        except KeyError:
+            raise AttributeError(name)
+
+        module = importlib.import_module(module, __name__)
+        attrib = getattr(module, name)
+        setattr(self, name, attrib)
+        return attrib
+
+
+wrapper = sys.modules[__name__] = LazyImporter(sys.modules[__name__])
+wrapper.add('.api', 'run_api')
+wrapper.add('.exploitservice', 'run_exploitservice')
+wrapper.add('.flagservice', 'run_flagservice')
+wrapper.add('.targets', 'run_targetservice')
+del wrapper  # remove wrapper reference
+del LazyImporter
